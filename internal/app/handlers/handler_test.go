b2d9eb110402274cb5f043ee3e8b9d7517e696f5
Saby <kotofeykoty@gmail.com>
2022-03-27 15:19:27 +0300

    sprint3inc8,9

--- internal/app/handlers/handler_test.go
+++ internal/app/handlers/handler_test.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/Fedorova199/red-cat/internal/app/middlewares"
 	"github.com/Fedorova199/red-cat/internal/app/storage"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
