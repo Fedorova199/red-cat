dbdb030578aef09309ad8ac0fbe86674f92e3143
Saby <kotofeykoty@gmail.com>
2022-03-27 15:52:33 +0300

    sprint3inc8,9,10,11,12,13

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -12,31 +12,38 @@ import (
 	"github.com/Fedorova199/red-cat/internal/app/handlers"
 	"github.com/Fedorova199/red-cat/internal/app/middlewares"
 	"github.com/Fedorova199/red-cat/internal/app/storage"
+	"github.com/Fedorova199/red-cat/internal/interfaces"
 	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
 func main() {
-	cfg, _ := config.NewConfig()
-
+	cfg, err := config.NewConfig()
+	if err != nil {
+		log.Fatalln(err)
+	}
 	db, err := sql.Open("pgx", cfg.DatabaseDSN)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln(err)
 	}
 	defer db.Close()
+
 	storage, err := storage.CreateDatabase(db)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln(err)
 	}
-	ms := []handlers.Middleware{
-		middlewares.GzipHandle{},
-		middlewares.UngzipHandle{},
-		middlewares.NewAuthenticator([]byte("secret key")),
+
+	ms := []interfaces.Middleware{
+		middlewares.GzipEncoder{},
+		middlewares.GzipDecoder{},
+		middlewares.NewAuth([]byte("secret key")),
 	}
+
 	handler := handlers.NewHandler(storage, cfg.BaseURL, ms)
 	server := &http.Server{
 		Addr:    cfg.ServerAddress,
 		Handler: handler,
 	}
+
 	c := make(chan os.Signal, 1)
 	signal.Notify(c,
 		syscall.SIGHUP,
